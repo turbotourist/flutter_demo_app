0b9fa10970ad0c7627c9e9b5dd61fa779b97e577
turbotourist <glaciefer.1@gmail.com>
2021-04-22 12:12:39 +0300

    add "style"

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,23 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(appBar: AppBar(title: Text("Flutter Demo"), centerTitle: true,),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Demo"),
+          centerTitle: true,
+        ),
+        body: Text(
+          "Hello, world",
+          style: TextStyle(fontSize: 30.0,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.underline,
+              letterSpacing: 3.0,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text("Add"),
+        ),
       ),
     ),
   );
