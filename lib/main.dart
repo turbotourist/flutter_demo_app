0fa25bcc948449e0d9ff1504a3ec03f3fcbcfb58
turbotourist <glaciefer.1@gmail.com>
2021-04-23 12:45:14 +0300

    RichText, children

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,37 @@ void main() {
           centerTitle: true,
           backgroundColor: Colors.grey[900],
         ),
-        body: Text(
-          "Hello, world",
-          style: TextStyle(fontSize: 30.0,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.bold,
-              decoration: TextDecoration.underline,
-              letterSpacing: 3.0,
-              color: Colors.teal,
-              fontFamily: "ZenDots"
+        body: RichText(
+          text: TextSpan(
+            style: TextStyle(
+                fontSize: 30.0,
+                fontStyle: FontStyle.italic,
+                //fontWeight: FontWeight.bold,
+                //decoration: TextDecoration.underline,
+                //letterSpacing: 3.0,
+                color: Colors.teal,
+                fontFamily: "ZenDots"),
+            children: <TextSpan>[
+              TextSpan(text: "Hello,"),
+              TextSpan(text: "World"),
+              TextSpan(text: "!"),
+            ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
           child: Text("Add"),
-            backgroundColor: Colors.grey[900],
+          backgroundColor: Colors.grey[900],
         ),
-          backgroundColor: Colors.amberAccent,
+        backgroundColor: Colors.amberAccent,
       ),
     ),
   );
+}
+
+class FlutterDemoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
 }
